68d9b47e4500fbce274837f66256e17e0a1ed3c5
Jacek Modrakowski <jacek.beny@gmail.com>
2017-06-23 12:53:45 +0200

    Adds engine to activity.

--- app/src/main/kotlin/io/scalac/android/circlegame/ui/MainActivity.kt
+++ app/src/main/kotlin/io/scalac/android/circlegame/ui/MainActivity.kt
@@ -3,11 +3,20 @@ package io.scalac.android.circlegame.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.scalac.android.circlegame.R
+import io.scalac.android.circlegame.engine.Engine
+import io.scalac.android.circlegame.engine.EngineView
+import io.scalac.android.circlegame.model.CircleModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EngineView {
+
+    private val engine: Engine by lazy { Engine(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showCircle(circle: CircleModel) {
+
     }
 }

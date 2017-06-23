dd00a1a48af54f9a48c0ad8b180e1e8faa636eb7
Jacek Modrakowski <jacek.beny@gmail.com>
2017-06-23 12:45:16 +0200

    Initial commit.

---
+++ app/src/main/kotlin/io/scalac/android/circlegame/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.scalac.android.circlegame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
